1105d13c9fed33adcc0e7e19cd1cdaf1d10f4e78
Shane Standifur <shanestandifur@gmail.com>
2023-06-13 15:02:50 -0500

    models created for backend for entertain me

---
+++ models/Venue.cs
@@ -0,0 +1,10 @@
+public class Venue
+{
+    public int Id {get;set;}
+    public string? Name {get;set;}
+    public string? Address {get;set;}
+    public string? CityName {get;set;}
+    public string? StateName {get;set;}
+    public string? VenuePagePicture {get;set;}
+
+}
